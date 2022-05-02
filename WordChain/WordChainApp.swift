4ed69844accebdd02dccbdc8865b935fc8b02956
Adam Anderson <adamcanderson400@gmail.com>
2022-05-02 17:21:13 -0400

    Initial Commit

---
+++ WordChain/WordChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordChainApp.swift
+//  WordChain
+//
+//  Created by Adam Anderson on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WordChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordChainUITests/WordChainUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WordChainUITestsLaunchTests.swift
+//  WordChainUITests
+//
+//  Created by Adam Anderson on 5/2/22.
+//
+
+import XCTest
+
+class WordChainUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
